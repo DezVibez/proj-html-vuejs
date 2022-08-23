748faba91249941b929fdfafb4214ac3e4fee433
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-08-23 23:34:58 +0200

    meet section no cards

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,11 +1,11 @@
 <template>
 
-  <div class="main-section">
-    <div class="my-container mt-5">
-      <div class="d-flex justify-content-center">
+  <div class="meet-section">
+    <div class="my-container">
+      <div class=" mx-auto pt-5">
   
         <div>
-          <h2 class="text-center mb-3">
+          <h2 class="text-center mb-3 mx-auto">
             <div>
               Meet The New Agency SEO Template 
             </div>
@@ -14,12 +14,33 @@
             </div>
           </h2>
     
-          <p class="w-75 text-center m-auto">
+          <p class="text-center mx-auto w-50">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur obcaecati tenetur eius accusamus 
           </p>
+        </div>
             
           
+
+
+        <div class="d-flex justify-content-between mb-5">
+          <div class="text-center border p-5  meet-section-card bg-white">
+
+          </div>
+
+          <div class="text-center border p-5 meet-section-card bg-white">
+
+          </div>
+
+          <div class="text-center border p-5 meet-section-card bg-white">
+
+          </div>
         </div>
+
+        <div class="my-table">
+
+        </div>
+
+          
       </div>
     </div>
 
@@ -40,13 +61,25 @@ export default {
 
 @import "../assets/sass/vars";
 
-.main-section{
-  height: 500px;
+.meet-section{
+  background-color: $meet_bg;
 }
 
 .my-container {
   max-width: 70%;
   margin: 0 auto;
+}
+
+.meet-section-card{
+width: 32%;
+}
+
+.my-table{
+  height: 420px;
+  background-image: url(../assets/images/agency-seo-desk-front-800x380.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: -2rem;
 }
 
 </style>
