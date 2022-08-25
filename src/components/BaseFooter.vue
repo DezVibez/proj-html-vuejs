966a602fa35ba0cab02ed880fc0794b5f47ab865
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-08-25 08:15:09 +0200

    iniziato footer

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="hello">
+  <div class="last-section">
 
-    <ul class="d-flex">
-      <li class="mx-3" v-for="(listItem, index) in list" :key="index">{{listItem}}</li>
-    </ul>
+    <div class="little-container border">
+
+      <div class="footer-logo my-4">
+        <img class="img-fluid" src="../assets/images/logo_seo_1x.png" alt="">
+      </div>
+
+      <ul class="d-flex general-font justify-content-between">
+        <li class="" v-for="(listItem, index) in list" :key="index">{{listItem}}</li>
+        <li>Get In Touch Now</li>
+      </ul>
+    </div>
+
+    
 
     
     
@@ -21,7 +31,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
 @import "../assets/sass/vars";
+
+.last-section{
+  height: 300px;
+
+  li{
+    color:$btn_gray;
+  }
+
+  li:hover{
+    color: $primary_col;
+  }
+
+  .little-container{
+    margin: 0 auto;
+    max-width: 50%;
+
+    .footer-logo{
+      margin: 0 auto;
+      width: 150px;
+    }
+  }
+}
+
 
 </style>
--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -610,7 +610,6 @@ export default {
     width: 100%;
     height:100%;
     background-color: rgba(0, 0, 0, 0.6)
-    
   }
 }
 </style>
